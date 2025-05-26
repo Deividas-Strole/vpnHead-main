2af6787b13c5715c43446415440bab934e9e81ba
deividas strole <deiwis@yahoo.com>
2025-05-26 10:12:10 -0700

    slider improvements

--- vpnHead/src/components/Slider.jsx
+++ vpnHead/src/components/Slider.jsx
@@ -3,9 +3,11 @@ import "../css/Slider.css";
 import ArticalSliderBox from "./ArticalSliderBox";
 
 const texts = [
-  "Privacy from Snooping Eyes",
-  "Privacy from Snooping rrrr",
-  "Privacy from Snooping uuuu",
+  "AAAAA",
+  "BBBB",
+  "CCCC",
+  "DDDD",
+  "EEEEE",
   // add more here
 ];
 
@@ -13,27 +15,25 @@ const items = texts.map((text, i) => (
   <ArticalSliderBox text={text} key={i} />
 ));
 
-
 export default function Slider() {
-  const [startIndex, setStartIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
-    setStartIndex((prev) => (prev + 1) % items.length);
+    setCurrentIndex((prev) => (prev + 1) % items.length);
   };
 
   const prevSlide = () => {
-    setStartIndex((prev) => (prev - 1 + items.length) % items.length);
+    setCurrentIndex((prev) => (prev - 1 + items.length) % items.length);
   };
 
   useEffect(() => {
-    const interval = setInterval(nextSlide, 3000);
+    const interval = setInterval(nextSlide, 2000);
     return () => clearInterval(interval);
   }, []);
 
-  const visibleItems = [];
-  for (let i = 0; i < 3; i++) {
-    visibleItems.push(items[(startIndex + i) % items.length]);
-  }
+  // Create an extended array for smooth infinite scrolling
+  const extendedItems = [...items, ...items, ...items];
+  const translateX = -((currentIndex + items.length) * (100 / 3));
 
   return (
     <div className="slider-container">
@@ -41,11 +41,16 @@ export default function Slider() {
         &#8249;
       </button>
       <div className="slider">
-        {visibleItems.map((item, index) => (
-          <div className="slide" key={index}>
-            {item}
-          </div>
-        ))}
+        <div 
+          className="slider-track" 
+          style={{ transform: `translateX(${translateX}%)` }}
+        >
+          {extendedItems.map((item, index) => (
+            <div className="slide" key={index}>
+              {item}
+            </div>
+          ))}
+        </div>
       </div>
       <button className="arrow right" onClick={nextSlide}>
         &#8250;
