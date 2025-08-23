02aaac6f2d468021ce34f9af5e7ec8ad20e83260
deividas strole <deiwis@yahoo.com>
2025-08-22 20:11:40 -0700

    changed  h1 color to red in AdminDash

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -7,7 +7,6 @@ const AdminDashboard = () => {
     return (
         <div className="centered-container">
             <h1>AdminDashboard</h1>
-            <p>AdminDashboard content</p>
             <Link to="/admin/article-editor" className="bg-blue-500 text-white px-4 py-2 rounded">Create New Article</Link>
         </div>
     );
