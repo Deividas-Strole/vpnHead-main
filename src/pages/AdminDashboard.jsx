53f7b4d0e4aaf3b0d638434232ef712ff16ff6d6
deividas strole <deiwis@yahoo.com>
2025-11-10 20:19:26 -0800

    created Go Back To Home Page link

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -8,6 +8,7 @@ const AdminDashboard = () => {
         <div className="centered-container">
             <h1>AdminDashboard</h1>
             <Link to="/admin/article-editor" className="create-article-link">Create New Article</Link>
+            <Link to="/" className="create-article-link">Go Back To Home Page</Link>
         </div>
     );
 }
